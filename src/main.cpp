975492a268636cec940063ebd813749a54a4fbb6
parker <parker@squarepegtech.com>
2024-08-15 11:05:38 -0400

    debugging facedetector

--- src/main.cpp
+++ src/main.cpp
@@ -147,11 +147,11 @@ void codeThreadProcessV(GoblinData &data) {
         // Wrap the raw data in OpenCV frame and show on screen
         cv::Mat frame(imH, imW, CV_8UC3, (void *) m.data);
 
-        MY_ASSERT(auto rectangles = face_detector.detect_face_rectangles(frame));
+        MY_ASSERT(auto rectangles = face_detector.detect_face_rectangles(frame);
         cv::Scalar color(0, 105, 205);
         for(const auto & r : rectangles){
             cv::rectangle(frame, r, color, 4);
-        }
+        })
         //cv::imshow("frame", frame);
         int key = cv::waitKey(1);
 
