3ffcc3567328e6e674ca00c9d01fd4c9816ff23f
parker <parker@squarepegtech.com>
2024-08-15 11:41:26 -0400

    debug message

--- src/main.cpp
+++ src/main.cpp
@@ -146,9 +146,9 @@ void codeThreadProcessV(GoblinData &data) {
         FaceDetector face_detector;
         // Wrap the raw data in OpenCV frame and show on screen
         cv::Mat frame(imH, imW, CV_8UC3, (void *) m.data);
-
         auto rectangles = face_detector.detect_face_rectangles(frame);
         cv::Scalar color(0, 105, 205);
+        cout << "pass" << endl;
         for(const auto & r : rectangles){
             cv::rectangle(frame, r, color, 4);
         }
