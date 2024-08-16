53cdf36c662827ddbb7bfc803035e782b4600746
team <parksandrecs@gmail.com>
2024-08-16 15:05:01 -0400

    testing imwrite on edge

--- src/FaceDetector.cpp
+++ src/FaceDetector.cpp
@@ -43,7 +43,7 @@ std::vector<cv::Rect> FaceDetector::detect_face_rectangles(const cv::Mat &frame)
         int y_right_top = static_cast<int>(detection_matrix.at<float>(i, 6) * frame.rows);
 
         cv::Mat croppedImage = frame(Rect(x_left_bottom, y_left_bottom, (x_right_top - x_left_bottom), (y_right_top - y_left_bottom)));
-        cv::imwrite("../../images/0.jpg", croppedImage)
+        cv::imwrite("../../images/0.jpg", croppedImage);
 
         faces.emplace_back(x_left_bottom, y_left_bottom, (x_right_top - x_left_bottom), (y_right_top - y_left_bottom));
     }
