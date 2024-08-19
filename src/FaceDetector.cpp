4a139c85bd3a6980ba6cbb3e2670e7911198a32f
team <parksandrecs@gmail.com>
2024-08-19 16:12:24 -0400

    testing imwrite on edge

--- src/FaceDetector.cpp
+++ src/FaceDetector.cpp
@@ -27,9 +27,11 @@ std::vector<cv::Rect> FaceDetector::detect_face_rectangles(const cv::Mat &frame)
 {
     cv::Mat input_blob = cv::dnn::blobFromImage(frame, scale_factor_, cv::Size(input_image_width_, input_image_height_),
                                                 mean_values_, false, false);
+    
     network_.setInput(input_blob, "data");
+    cv::Mat detection = network_.forward("detection_out");
     cv::Mat detection_matrix(detection.size[2], detection.size[3], CV_32F, detection.ptr<float>());
-    cout << "detection: " << detection_matrix << endl;
+
     std::vector<cv::Rect> faces;
 
     for (int i = 0; i < detection_matrix.rows; i++) {
@@ -45,9 +47,9 @@ std::vector<cv::Rect> FaceDetector::detect_face_rectangles(const cv::Mat &frame)
         int y_right_top = static_cast<int>(detection_matrix.at<float>(i, 6) * frame.rows);
 
         faces.emplace_back(x_left_bottom, y_left_bottom, (x_right_top - x_left_bottom), (y_right_top - y_left_bottom));
-        cout << "blob: " << input_blob << endl;
-        cv::Mat detection = network_.forward("detection_out");
         //cout << detection_matrix << endl;
+        cout << "blob: " << input_blob << endl;
+        cout << "detection: " << detection_matrix << endl;
     }
 
     
