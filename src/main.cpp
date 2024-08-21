8552f0c609159e99747bf137b8502793f8b2bb80
team <parksandrecs@gmail.com>
2024-08-21 13:19:52 -0400

    testing imwrite on edge

--- src/main.cpp
+++ src/main.cpp
@@ -156,7 +156,7 @@ void codeThreadProcessV(GoblinData &data) {
             ROI.copyTo(croppedImage);
             if(!croppedImage.empty())
             {
-                cout << croppedImage.size()
+                cout << croppedImage.size();
                 //croppedImage.reshape(1,3,260,260);
                 // Declare what you need
                 cv::FileStorage file("../../images/" + std::to_string(n), cv::FileStorage::WRITE);
