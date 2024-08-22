5aeaac3fafcebe6996f9cf6d3c6d687a29b595e0
team <parksandrecs@gmail.com>
2024-08-22 15:42:04 -0400

    testing imwrite on edge

--- src/main.cpp
+++ src/main.cpp
@@ -194,7 +194,7 @@ void codeThreadProcessV(GoblinData &data) {
                 //preprocess cropped image here for enet.dlc 
                 cv::Mat cropped_blob = cv::dnn::blobFromImage(ROI,1,cv::Size(sw,sh), cv::INTER_LINEAR,
                                              false);
-                cv::Mat cropped_blob_transposed;
+                cropped_blob.copyto(cv::Mat cropped_blob_transposed);
                 chw_to_hwc(cropped_blob, cropped_blob_transposed);
 
                 // Declare what you need
