11c296558db347cd463af112f81614e1c73340a0
team <parksandrecs@gmail.com>
2024-08-27 10:01:49 -0400

    testing imwrite on edge

--- src/main.cpp
+++ src/main.cpp
@@ -171,7 +171,7 @@ void codeThreadProcessV(GoblinData &data) {
                 for ( int i=0;i<sh;i++) {
                     for ( int j=0;j<sw;j++) {
                         for ( int k=0;k<3;k++) {
-                            raw[0][i][j][k] = cropped_blob.at<float>(0,i,j,k);
+                            raw[0][i][j][k] = cropped_blob.at<Vec3b>(0,i,j,k);
                         }
                     }
                 } 
