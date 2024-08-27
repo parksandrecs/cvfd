9194b71fd110bca5a0fe122a00b91df2f3f7709e
team <parksandrecs@gmail.com>
2024-08-27 15:03:16 -0400

    testing imwrite on edge

--- src/create_raws.py
+++ src/create_raws.py
@@ -43,7 +43,7 @@ def __create_mean_raw(img_raw, mean_rgb):
     mean_raw = np.transpose(mean_raw, (1, 2, 0))
     return mean_raw.astype(np.float32)
 
-def __create_raw_incv3(img_filepath, mean_rgb, div, req_bgr_raw, save_uint8,out_filepath):
+def __create_raw_incv3(img_filepath, mean_rgb, div, req_bgr_raw, save_uint8):
     img_raw = __get_img_raw(img_filepath)
     mean_raw = __create_mean_raw(img_raw, mean_rgb)
 
@@ -57,33 +57,75 @@ def __create_raw_incv3(img_filepath, mean_rgb, div, req_bgr_raw, save_uint8,out_
 
     if save_uint8:
         snpe_raw = snpe_raw.astype(np.uint8)
+        print(snpe_raw)
     else:
         snpe_raw = snpe_raw.astype(np.float32)
 
     img_filepath = os.path.abspath(img_filepath)
-    filepath, ext = os.path.splitext(img_filepath)
-    path, filename = os.path.split(filepath)
-    snpe_raw_filename = out_filepath + "/" + filename
+    filename, ext = os.path.splitext(img_filepath)
+    snpe_raw_filename = filename
     snpe_raw_filename += '.raw'
     snpe_raw.tofile(snpe_raw_filename)
 
     return 0
 
+def __resize_square_to_jpg(src, dst, size,resize_type):
+    src_img = Image.open(src)
+    # If black and white image, convert to rgb (all 3 channels the same)
+    if len(np.shape(src_img)) == 2: src_img = src_img.convert(mode = 'RGB')
+    # center crop to square
+    width, height = src_img.size
+    short_dim = min(height, width)
+    crop_coord = (
+        (width - short_dim) / 2,
+        (height - short_dim) / 2,
+        (width + short_dim) / 2,
+        (height + short_dim) / 2
+    )
+    img = src_img.crop(crop_coord)
+    # resize to inceptionv3 size
+    if resize_type == RESIZE_METHOD_BILINEAR :
+        dst_img = img.resize((size, size), Image.BILINEAR)
+    else :
+        dst_img = img.resize((size, size), Image.ANTIALIAS)
+    # save output - save determined from file extension
+    dst_img.save(dst)
+    return 0
+
+def convert_img(src,dest,size,resize_type):
+    print("Converting images for inception v3 network.")
+
+    print("Scaling to square: " + src)
+    path, filename = os.path.split(src)
+    dest_image = os.path.join(dest, filename)
+    __resize_square_to_jpg(src,dest_image,size,resize_type)
+
+    print("Image mean: " + dest)
+    mean_rgb=(128,128,128)
+    __create_raw_incv3(dest_image,mean_rgb,128,False,False)            
+
 
 def main():
     parser = argparse.ArgumentParser(description="Batch convert jpgs",formatter_class=argparse.ArgumentDefaultsHelpFormatter)
     parser.add_argument('-d', '--dest',type=str, required=True)
-    parser.add_argument('-i','--img_file',type=str, required=True)
+    parser.add_argument('-s','--size',type=int, default=299)
+    parser.add_argument('-i','--img_folder',type=str, required=True)
+    parser.add_argument('-r','--resize_type',type=str, default=RESIZE_METHOD_BILINEAR,
+                        help='Select image resize type antialias or bilinear. Image resize type should match '
+                             'resize type used on images with which model was trained, otherwise there may be impact '
+                             'on model accuracy measurement.')
 
     args = parser.parse_args()
-    
-    src = os.path.abspath(args.img_file)
+
+    size = args.size
+    src = os.path.abspath(args.img_folder)
     dest = os.path.abspath(args.dest)
+    resize_type = args.resize_type
 
-    mean_rgb=(128,128,128)
-    __create_raw_incv3(src,mean_rgb,128,False,False,dest)
+    assert resize_type == RESIZE_METHOD_BILINEAR or resize_type == RESIZE_METHOD_ANTIALIAS, \
+           "Image resize method should be antialias or bilinear"
 
-
+    convert_img(src,dest,size,resize_type)
 
 if __name__ == '__main__':
     exit(main())
